622eaa612a72efcdd143341d195ef4d0b9697439
RBDev <rossebrown@gmail.com>
2026-01-26 10:15:11 -0500

    chore(04-02): export ThreadCommands from src/thread package
    - Added ThreadCommands to __all__ in src/thread/__init__.py
    - Enables clean imports: 'from src.thread import ThreadCommands'
    - Verified import works correctly

--- src/thread/__init__.py
+++ src/thread/__init__.py
@@ -5,5 +5,11 @@ Provides persistent storage for Signal thread to project directory associations.
 """
 
 from .mapper import ThreadMapper, ThreadMapping, ThreadMappingError
+from .commands import ThreadCommands
 
-__all__ = ["ThreadMapper", "ThreadMapping", "ThreadMappingError"]
+__all__ = [
+    "ThreadMapper",
+    "ThreadMapping",
+    "ThreadMappingError",
+    "ThreadCommands"
+]
